cfe77ad84f03e8cfbc918a4fb549d970d03e7e41
rsjolo <joose.lohi@tuni.fi>
2022-08-30 04:38:59 +0300

    add quicksort.hs

---
+++ quicksort.hs
@@ -0,0 +1,8 @@
+-- Implementation of the quick sort algorithm.
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+  let smallerSorted = quicksort [a | a <- xs, a <= x]
+      biggerSorted = quicksort [a | a <- xs, a > x]
+  in  smallerSorted ++ [x] ++ biggerSorted
